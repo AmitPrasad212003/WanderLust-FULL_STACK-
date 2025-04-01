f2d77ad9686f2d8d29acf779bc78f7df1d41a374
AmitPrasad212003 <amitprasad212003@gamil.com>
2025-04-01 16:05:18 +0530

    add nav bar

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodoverride = require("method-override");
-const { log } = require("console");
+const ejsMate = require("ejs-mate");
  
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -24,7 +24,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended : true}));
 app.use(methodoverride("_method"));
-
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 
 
