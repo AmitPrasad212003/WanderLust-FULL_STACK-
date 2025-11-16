8db82981da5f0fca029c5025fe10850d8ffd2c4e
AmitPrasad212003 <amitprasad212003@gmail.com>
2025-11-16 16:30:52 +0530

    edit image upload

--- controllers/listings.js
+++ controllers/listings.js
@@ -42,13 +42,22 @@ module.exports.editedListing = async(req, res) => {
         req.flash("error", "Listing you requested for does not exits");
         res.redirect("/listing");
     }
-    res.render("listing/edit.ejs", { listing });
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_200,w_300");
+    res.render("listing/edit.ejs", { listing , originalImageUrl});
 };
 
 module.exports.updateListing =async (req, res) => {
     
     let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
+
+    if(typeof req.file != "undefined"){
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = {url, filename};
+        await listing.save();
+    }
     req.flash("success", "Listing Updated!");
     res.redirect(`/listing/${id}`);
 };
